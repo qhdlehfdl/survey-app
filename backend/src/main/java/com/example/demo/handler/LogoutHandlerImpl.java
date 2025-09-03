c313b8f51bd3110c9fb2222737cdc5623ba760eb
qhdlehfdl <brian5968@naver.com>
2025-09-03 16:19:24 +0900

    cookie delete in logout

--- backend/src/main/java/com/example/demo/handler/LogoutHandlerImpl.java
+++ backend/src/main/java/com/example/demo/handler/LogoutHandlerImpl.java
@@ -1,6 +1,8 @@
 package com.example.demo.handler;
 
 import com.example.demo.auth.service.RefreshTokenService;
+import com.example.demo.common.ResponseCode;
+import com.example.demo.common.ResponseMessage;
 import com.example.demo.token.JwtProvider;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -8,6 +10,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.util.StringUtils;
@@ -15,6 +20,8 @@ import org.springframework.util.StringUtils;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
 @RequiredArgsConstructor
 public class LogoutHandlerImpl implements LogoutHandler {
@@ -40,6 +47,24 @@ public class LogoutHandlerImpl implements LogoutHandler {
             if(!isValid) return;
 
             refreshTokenService.deleteToken((Integer) authentication.getPrincipal());
+
+            ResponseCookie cookie = ResponseCookie.from("refreshToken", "")
+                    .httpOnly(true).secure(true)
+                    .path("/api/auth")
+                    .maxAge(0)
+                    .sameSite("Strict").build();
+            response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
+
+            Map<String, Object> responseBody = new HashMap<>();
+            responseBody.put("code", ResponseCode.SUCCESS);
+            responseBody.put("message", ResponseMessage.SUCCESS);
+
+            response.setStatus(HttpStatus.OK.value());
+            response.setContentType("application/json;charset=UTF-8");
+
+            String json = objectMapper.writeValueAsString(responseBody);
+            response.getWriter().write(json);
+            response.getWriter().flush();
 
         } catch (IOException e) {
             throw new RuntimeException("Failed to read refresh token");
